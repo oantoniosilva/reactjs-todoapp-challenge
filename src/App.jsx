cf35ca79ac7fdaca41ab29563770fe7029f2d0fb
Antonio <antoniopsilva083@gmail.com>
2023-02-07 23:48:43 -0300

    refact: refact app.module.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,91 @@
+import './global.css'
+import styles from "./App.module.css";
+
 import { Header } from './components/Header';
-import { NewTask } from './components/NewTask';
+import { AiOutlinePlusCircle } from "react-icons/ai";
+import { useState } from 'react';
 import { Tasks } from './components/Tasks';
 
-import './global.css'
-
 export function App() {
+  const [tasks, setTasks] = useState([]);
+  const [newTaskText, setNewTaskText] = useState('');
+
+  const counterTasks = tasks.length;
+  // const completedTasks = tasks.filter((task) => task.isCompleted).length;
+
+  function handleCreateNewTask() {
+    event.preventDefault()
+    
+    setTasks([...tasks, newTaskText])
+    setNewTaskText('')
+  }
+  
+  function handleNewTaskChange() {
+
+    setNewTaskText(event.target.value)
+
+  }
+
+  function deleteTask(taskToDelete) {
+    const tasksWithoutDeleteOne = tasks.filter(task => {
+      return task !== taskToDelete;
+    })
+
+    setTasks(tasksWithoutDeleteOne)
+  }
+
   return (
     <>
       <Header />
-      <NewTask/>
+
+      <div className={styles.container}>
+        <form 
+          onSubmit={handleCreateNewTask}
+          className={styles.content}
+        >
+          <input
+            name="tasks" 
+            type="text" 
+            placeholder="Add a new task"
+            value={newTaskText} 
+            onChange={handleNewTaskChange}
+            // onInvalid={handleNewTaskInvalid}
+            required  
+          />
+
+          <button 
+            // onClick={handleCounterTask}
+            type="submit" 
+            // disabled={isNewTaskEmpty}
+          >
+            Create <AiOutlinePlusCircle size={16} color="#f2f2f2" title='Button add task' />
+          </button>
+        </form>
+
+        <div className={styles.preview}>
+          <div className={styles.create}>
+            <p>Created tasks</p>
+            <span>{counterTasks}</span>
+          </div>
+
+          <div className={styles.completed}>
+            <p>Completed</p>
+            <span>{} de {counterTasks}</span>
+          </div>
+        </div>
+
+        <div>
+          {tasks.map(task => {
+            return (
+              <Tasks 
+                key={task} 
+                content={task} 
+                onDeleteTask={deleteTask} 
+              />
+            )
+          })}
+        </div>
+      </div>
     </>
   )
 }
--- src/components/Tasks/index.jsx
+++ src/components/Tasks/index.jsx
@@ -3,13 +3,29 @@ import { AiOutlineCheckCircle } from "react-icons/ai";
 
 import styles from './Tasks.module.css';
 
-export function Tasks({ content }) {
+export function Tasks({ id, content, isCompleted, onDeleteTask }) {
+
+  function handleDeleteTask() {
+    onDeleteTask(content)
+  }
+
   return (
     <div className={styles.task}>
       <div className={styles.taskContent}>
-        <AiOutlineCheckCircle size={16} color="#808080" />
+        <button 
+          className={styles.buttonCheck}
+          onClick={isCompleted}
+        >
+          <AiOutlineCheckCircle size={16} color="#808080" />
+        </button>
           <p>{content}</p>
-        <BiTrash size={16} color="#808080" />
+        <button 
+          className={styles.buttonTrash}
+          title='Delete task' 
+          onClick={handleDeleteTask}
+        >
+          <BiTrash size={16} color="#808080" />
+        </button>
       </div>
     </div>
   )
